ce9860c4e248eb5b56e673e2b18035448076ef45
michaljarnot <michal.jarnot@strv.com>
2017-09-09 15:44:21 +0200

    Add eslint

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,18 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
   devServer: {
     contentBase: './public',
     port: 3000,
