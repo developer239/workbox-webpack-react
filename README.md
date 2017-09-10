fcede0f0644659d002a8693b2b021dfd5754847f
michaljarnot <michal.jarnot@strv.com>
2017-09-10 14:14:49 +0200

    Add readme files

---
+++ README.md
@@ -0,0 +1,21 @@
+# React PWA
+
+Simple progressive web app that takes advantage of new technologies to bring the best of mobile sites & native apps to users.
+
+Uses:
+
+ * Workbox
+ * Webpack
+ * React
+
+ Commands:
+ 
+ `yarn install` installs the project
+ 
+ `yarn dev` runs development server
+ 
+ `yarn build` compiles project into production ready files
+ 
+ `yarn server` runs http web server
+ 
+ `yarn prod` builds files && runs http server
--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import React from 'react'
 const App = () => (
   <div>
     <header>
-      <h2>Welcome to React</h2>
+      <h2>React PWA</h2>
     </header>
-    <article>Hello world!</article>
+    <article>
+      <p>
+        Simple progressive web app that takes advantage of new technologies
+        to bring the best of mobile sites & native apps to users.
+      </p>
+      <b>Uses:</b>
+      <ul>
+        <li>Workbox (workbox-webpack-plugin)</li>
+        <li>Webpack</li>
+        <li>React</li>
+      </ul>
+    </article>
   </div>
 )
 
