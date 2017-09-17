453d584d3f926d7926e3d35af9dafc162227e54e
michaljarnot <michal.jarnot@strv.com>
2017-09-17 20:56:29 +0200

    Replace http-server with express server

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT ? process.env.PORT : 8080
+const dist = path.join(__dirname, 'public')
+
+
+app.use(express.static(dist))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(dist, 'index.html'))
+})
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log(error) // eslint-disable-line no-console
+  }
+  console.info('Express is listening on port %s.', port) // eslint-disable-line no-console
+})
