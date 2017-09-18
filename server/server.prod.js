e5fbfaecef098ec7358162fec4ea3b7ed5687491
michaljarnot <michal.jarnot@strv.com>
2017-09-18 13:47:31 +0200

    Add vendor path to prod express server script

--- server/server.prod.js
+++ server/server.prod.js
@@ -8,6 +8,12 @@ const DIST_DIR = path.resolve(__dirname, '..', 'public')
 const app = express()
 
 app.use(express.static(DIST_DIR))
+
+// Create route for static vendors.js file
+app.get('/vendor/vendors.js', (req, res) => {
+  res.sendFile(`${DIST_DIR}/vendor/vendors.js`)
+})
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
 })
